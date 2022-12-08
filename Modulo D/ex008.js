f964e624239856b711b12377d113c204b805bc28
victorcarpinteiro <victor.jose.carpinteiro@gmail.com>
2022-12-08 16:13:50 -0300

    criando condições js

---
+++ Modulo D/ex008.js
@@ -0,0 +1,6 @@
+var vel = '61'
+console.log(`Sua velocidade foi de ${vel}Km/Hora`)
+if(vel>60){ //condição simples contendo apenas o se (if).
+    console.log('Ultrapassou a velocidade. Multado!')
+}
+console.log('Dirija sempre com cinto de segurança') 
