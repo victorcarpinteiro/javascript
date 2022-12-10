98815d901e274c57ac30e25f2d2d3f69510d0fac
victorcarpinteiro <victor.jose.carpinteiro@gmail.com>
2022-12-10 18:15:30 -0300

    repetições em JS

--- Modulo D/exercicios/ex perfil/script.js
+++ Modulo D/exercicios/ex perfil/script.js
@@ -1,27 +1,49 @@
 function verificar(){
     var data = new Date()
-    var anoAtual = data.getFullYear()
-    var nasc = document.querySelector('input#ano')
+    var ano = data.getFullYear()
+    var formAno = document.querySelector('input#ano')
     var res = document.querySelector('div#verificador')
-    
-    if (nasc.value < 1900 || nasc.value >= anoAtual){
-        window.alert('[ERRO] Preencha os dados corretamente')
-    } else{
-        var fsex = document.getElementsByName('radsex')
-        var idade = anoAtual - Number(nasc.value)
-
-        var genero = ''
-        if(fsex[0].checked){
+    if (formAno.value <= 0 || formAno.value >= ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente! ')
+    }else{
+         var sex = document.getElementsByName('radsex')
+         var idade = ano - formAno.value
+         var genero = ''
+         var img = document.createElement('img')
+         img.setAttribute('id', 'fotos') 
+         if (sex[0].checked){
             genero = 'Homem'
-        } else if(fsex[1].checked){
+            if (idade >=0 && idade <10){
+                // criança
+                img.setAttribute('src','imagens/criancah.jpg') 
+            } else if (idade <21){
+                //joven
+                img.setAttribute('src','imagens/jovenh.jpg') 
+            } else if (idade <50){
+                //adulto
+                img.setAttribute('src','imagens/adultoh.jpg') 
+            }else{
+                //idoso
+                img.setAttribute('src','imagens/idosoh.jpg') 
+            }
+         } else if(sex[1].checked){ 
             genero = 'Mulher'
-        }
-        res.innerHTML = `Detectado ${genero} com ${idade} anos.`
-    } if(idade >= 5 && idade <=12 && genero = 'Homem'){
-        window.alert('criança homem')
-    } else if(idade >= 5 && idade <=12 && genero = 'Mulher'){
-        window.alert('criança mulher')
-    }  if(idade >=13 && idade <= 18){
-        window.alert('adolescente')
+            if (idade >=0 && idade <10){
+                // criança
+                img.setAttribute('src','imagens/criancam.jpg') 
+            } else if (idade <21){
+                //joven
+                img.setAttribute('src','imagens/jovenm.jpg') 
+            } else if (idade <50){
+                //adulto
+                img.setAttribute('src','imagens/adultom.jpg') 
+            }else{
+                //idoso
+                img.setAttribute('src','imagens/idosam.jpg') 
+            }
+         }
+         res.style.textAlign = 'center'
+         res.innerHTML = `Detectamos ${genero} com ${idade} idade.`
+         res.appendChild(img)
     }
 }
---
+++ Modulo E/ex015.js
@@ -0,0 +1,18 @@
+// var c = 1
+// while (c <= 10){
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+// var c = 1
+
+// do{
+//     console.log(`Passo ${c}`)
+//     c++
+// }while(c <= 10)
+
+console.log('vai começar...')
+for(var c = 1; c<= 10;c++){
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
