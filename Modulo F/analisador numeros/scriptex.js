4fb81e462ca563d52423df32d9ef2924778c283d
victorcarpinteiro <victor.jose.carpinteiro@gmail.com>
2022-12-23 23:04:04 -0300

    finalizando curso js

--- Modulo F/analisador numeros/scriptex.js
+++ Modulo F/analisador numeros/scriptex.js
@@ -8,10 +8,11 @@ function adicionar(){
     } else {
         var ex = vetor.indexOf(numero.value)
         if(vetor == [] || ex == -1){
-        vetor.push(numero.value)
+        vetor.push(Number(numero.value))
         var num = document.createElement('option')
         num.innerHTML = `Valor ${numero.value} adicionado`
         campoVetor.appendChild(num)
+        texto.innerHTML = ''
         }else{
         window.alert('[Erro] Número já inserido')
     }
@@ -19,18 +20,33 @@ function adicionar(){
 }
 
 function finalizar(){
-    var organizar = vetor.sort()
-    var quantidade = vetor.length
-    var maior 
-    var menor = vetor[0]
-    var soma
-    // var média // soma dividido pela quantidade
-    // window.alert(maior)
-    // window.alert(s)
-    var qtt = document.createElement('p')
-    qtt.innerHTML = `Ao todo temos ${quantidade} números cadastrados.`
-    texto.appendChild(qtt)
-    var meno = document.createElement('p')
-    meno.innerHTML = `O menor valor informado foi ${menor}.`
-    texto.appendChild(meno)
+
+    if(vetor == 0){
+        window.alert('Favor preenhcer um numero')
+    }else{
+        
+        var total = vetor.length
+        var maior = vetor[0]
+        var menor = vetor[0]
+        var soma = 0
+        var media = 0
+
+        for(var pos in vetor){
+            soma += vetor[pos]
+            if(vetor[pos] > maior){
+                maior = vetor[pos]
+            }
+            if(vetor[pos] < menor){
+                menor = vetor[pos]
+            }
+        }
+        media = soma / total
+        texto.innerHTML = ''
+        texto.innerHTML += `<p> Ao todo, temos ${total} números cadastrados <\p>`
+        texto.innerHTML += `<p> O menor número é ${menor}<\p>`
+        texto.innerHTML += `<p> O maior número é ${maior}<\p>`
+        texto.innerHTML += `<p> A soma dos valores é ${soma}<\p>`
+        texto.innerHTML += `<p> A média dos valores é ${media}<\p>`
+    }
+    
 }
